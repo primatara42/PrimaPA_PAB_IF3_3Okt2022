cce84de3cac8e4ab80c857d690ac0a211a215dde
primatara42 <primatara42@gmail.com>
2022-10-03 11:20:38 +0700

    Commit Lagi

--- app/src/main/java/com/primapa_pab_if3/MainActivity.java
+++ app/src/main/java/com/primapa_pab_if3/MainActivity.java
@@ -6,17 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
 {
     Button btnPindah;
+    EditText etNama;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setTitle("Layout A");
+
         btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById(R.id.et_nama);
+
 
         btnPindah.setOnClickListener(new View.OnClickListener()
         {
@@ -25,7 +31,12 @@ public class MainActivity extends AppCompatActivity
             {
                 Intent pindahlayout = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(pindahlayout);
+
             }
+
         });
+
+
+
     }
 }
